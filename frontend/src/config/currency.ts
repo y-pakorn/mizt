db7968bdff5a5a4e4d34a8c1a0e1b1367528f6ad
Pakorn Nathong <pakorn.nth@hotmail.com>
2025-06-14 22:43:23 +0700

    feat: add multiple tokens

--- frontend/src/config/currency.ts
+++ frontend/src/config/currency.ts
@@ -24,6 +24,38 @@ export const CURRENCIES: Currency[] = [
       "0x356a26eb9e012a68958082340d4c4116e7f55615cf27affcff209cf0ae544f59::wal::WAL",
     decimals: 9,
   },
+  {
+    ticker: "haSUI",
+    name: "Haedal Staked Sui",
+    logo: "https://coin-images.coingecko.com/coins/images/33512/large/hasui.png?1740451722",
+    coinType:
+      "0xbde4ba4c2e274a60ce15c1cfff9e5c42e41654ac8b6d906a57efa4bd3c29f47d::hasui::HASUI",
+    decimals: 9,
+  },
+  {
+    ticker: "SuiNS",
+    name: "Sui Name Service",
+    logo: "https://token-image.suins.io/icon.svg",
+    coinType:
+      "0x5145494a5f5100e645e4b0aa950fa6b68f614e8c59e17bc5ded3495123a79178::ns::NS",
+    decimals: 6,
+  },
+  {
+    ticker: "BLUE",
+    name: "Bluefin",
+    logo: "https://bluefin.io/images/square.png",
+    coinType:
+      "0xe1b45a0e641b9955a20aa0ad1c1f4ad86aad8afb07296d4085e349a50e90bdca::blue::BLUE",
+    decimals: 9,
+  },
+  {
+    ticker: "DEEP",
+    name: "Deepbook",
+    logo: "https://images.deepbook.tech/icon.svg",
+    coinType:
+      "0xdeeb7a4662eec9f2f3def03fb937a663dddaa2e215b8078a284d026b7946c270::deep::DEEP",
+    decimals: 6,
+  },
 ]
 
 export const SUI_CURRENCY: Currency = CURRENCIES[0]!
